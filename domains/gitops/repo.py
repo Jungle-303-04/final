8dadf4021d8b16cf7881b54c03eddd8cd1cd8529
choi woo-nyong <woonyong.kr@gmail.com>
2026-06-29 18:58:11 +0000

    refactor: domains를 루트로 이동

--- domains/gitops/repo.py
+++ domains/gitops/repo.py
@@ -4,8 +4,8 @@ from __future__ import annotations
 
 from sqlalchemy.dialects.postgresql import insert as pg_insert
 
+from domains.gitops.tables import RepoChange
 from packages.contracts.event_bus.interfaces import JsonObject
-from packages.domains.gitops.tables import RepoChange
 from packages.storage.engine import DatabaseConnection
 
 
