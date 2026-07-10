0dafc8196423b4946696208bd000b3234e6dfcf1
minmings111 <minmings111@gmail.com>
2026-07-10 22:43:44 +0900

    feat: add referenced config object metadata evidence

--- tests/test_target_agent_commands.py
+++ tests/test_target_agent_commands.py
@@ -44,6 +44,7 @@ def load_agent_module():
         "providers.kubernetes_utils",
         "providers.kubernetes_providers",
         "providers.loki_providers",
+        "providers.metadata_config_objects",
         "providers.metadata_config_refs",
         "providers.metadata_endpoint_slices",
         "providers.metadata_ownership",
